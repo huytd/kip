6e53e45d7069452f219a372881eac622969f991a
Huy Tran <kingbazoka@gmail.com>
2016-07-31 20:34:20 -0700

    Open file to publish :v

--- lib/kip/cli.rb
+++ lib/kip/cli.rb
@@ -6,10 +6,12 @@ module Kip
     method_option :title, :aliases => "-t", :desc => "Title of your post", :required => true
     method_option :tags, :aliases => "-g", :desc => "Tag list, separated by a comma", :required => true
     method_option :publish, :aliases => "-p", :desc => "Publish or draft, true/false", :default => false
-    def post(file)
+    def post(filename)
       title = options[:title]
       tags = options[:tags]
       publish = options[:publish]
+      file = open(filename)
+      content = file.read
       puts "Title: " + title
       puts "Tags: " + tags
       puts "Submit as " + ((publish) ? "published post" : "a draft")
